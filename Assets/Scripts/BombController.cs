d6468d318a08676b0b45cfcd0f21067b0862a8fb
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2022-12-04 20:27:19 +0100

    added comments how BombController works

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -19,33 +19,35 @@ public class BombController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if number of bombs remaining is grater than 0 and put space key code
         if (bombsRemaining > 0 && Input.GetKeyDown(KeyCode.Space))
         {
+            //start coroutine of function
             StartCoroutine(PlaceBomb());
         }
     }
 
+    //special method type where placing bomb is in kinda loop? after all instructions here the old bomb dissapears
+    //and we can put bomb again
     private IEnumerator PlaceBomb()
     {
+        //save to new variable position from player
         Vector2 position = transform.position;
+
+        //round coordinates x, y so bomb is put on whole positions
         position.x = Mathf.Round(position.x);
         position.y = Mathf.Round(position.y);
 
+        //create instance of bomb with bomb prefab and rounded position
+        //so bomb can appear there
         GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
         bombsRemaining--;
 
+        //wait for bomb to fuse for specific amount of seconds
         yield return new WaitForSeconds(bombFuseTime);
 
         Destroy(bomb);
+        //after that bom can be put again
         bombsRemaining++;
     }
-
-    /*
-    private void OnTriggerExit2D(Collider2D collider)
-    {
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Bomb"))
-        {
-            collider.isTrigger = false;
-        }
-    }*/
 }
