f912c9cd3b162a2909fd3e13c0d5efbcd5115171
Klaudia Rutkowska <rutkowskaklaudia731@gmail.com>
2022-12-04 23:05:10 +0100

    Working on explosion

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 
 public class BombController : MonoBehaviour
 {
+    [Header("Bomb")]
     public GameObject bombPrefab;
     public float bombFuseTime = 5f;
     public int bombAmount = 1;
     private int bombsRemaining;
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public float explosionDuration = 1f;
+    public int explosionSize = 1;
 
     private void OnEnable()
     {
@@ -46,8 +52,25 @@ public class BombController : MonoBehaviour
         //wait for bomb to fuse for specific amount of seconds
         yield return new WaitForSeconds(bombFuseTime);
 
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+
+        Destroy(explosion.gameObject, explosionDuration);
+
+        Explode(position, Vector2.up, explosionSize);
+        Explode(position, Vector2.down, explosionSize);
+        Explode(position, Vector2.left, explosionSize);
+        Explode(position, Vector2.right, explosionSize);
+
         Destroy(bomb);
         //after that bom can be put again
         bombsRemaining++;
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
     }
 }
